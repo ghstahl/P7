4e9cd8b41d3525ed9e45814cd60dd9c391994436
ghstahl@gmail.com <ghstahl@gmail.com>
2017-04-26 10:43:28 -0700

    added riot views

---
+++ src/p7.main/Areas/Riot/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using p7.main.Models;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace p7.main.Areas.Riot.Controllers
+{
+   
+
+    [Area("Riot")]
+    public class HomeController : Controller
+    {
+        private ILogger Logger { get; set; }
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private ISession Session => _httpContextAccessor.HttpContext.Session;
+
+
+
+        public HomeController(IHttpContextAccessor httpContextAccessor,
+            ILogger<HomeController> logger)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            Logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Logger.LogInformation("Hello from the Home Index Controller");
+           
+            return View();
+        }
+
+    }
+}
