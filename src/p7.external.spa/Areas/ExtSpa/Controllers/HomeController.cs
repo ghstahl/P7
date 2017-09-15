43ee710be94157db2f3a37ed5f4b1e05c1d6a248
ghstahl@gmail.com <ghstahl@gmail.com>
2017-09-15 07:58:03 -0700

    more rich external razor

--- src/p7.external.spa/Areas/ExtSpa/Controllers/HomeController.cs
+++ src/p7.external.spa/Areas/ExtSpa/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using P7.External.SPA.Core;
 using P7.External.SPA.Models;
-
+using System.Linq;
 
 namespace P7.External.SPA.Areas.ExtSpa.Controllers
 {
@@ -31,16 +31,12 @@ namespace P7.External.SPA.Areas.ExtSpa.Controllers
         {
             Logger.LogInformation("Hello from the External SPA Home Index Controller");
             var spa = _externalSpaStore.GetRecord(id);
-            var model = new HtmlString(spa.RenderTemplate);
-            if (!string.IsNullOrEmpty(spa.View))
-            {
-                return View(spa.View);
-            }
-            else
-            {
-                return View(new SectionValue() { Value = model });
-            }
-        }
+            var result = HttpContext.User.Claims.Select(
+                c => new ClaimType {Type = c.Type, Value = c.Value});
 
+           // var model = new HtmlString(spa.RenderTemplate);
+
+            return View(spa.View, result);
+        }
     }
 }
--- src/p7.external.spa/Models/SectionValue.cs
+++ src/p7.external.spa/Models/SectionValue.cs
@@ -9,4 +9,9 @@ namespace P7.External.SPA.Models
     {
         public HtmlString Value { get; set; }
     }
+    public class ClaimType
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
 }
